1f8817b325e5f39621952975276b22da92f64819
Marco Venicio <marcovenicio1515@gmail.com>
2021-05-15 19:45:09 -0400

    commit

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+/** progress bar */
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function() {
+        let progressHeigtht = (window.pageYOffset / totalHeight) * 100;
+        progress.style.height = progressHeigtht + "%";
+    }
+    /** Menu principal */
+
+$(document).ready(function() {
+
+    var btnMenuMobile = $('.menu');
+
+    $('.btn-menumobile').on('click', function() {
+
+        $('.menu ul').toggleClass('open');
+
+    });
+
+});
+
+
+
+/**
+ * Galeria
+ */
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+/*vanilha */
+
+VanillaTilt.init(document.querySelectorAll(".box-parceiro"), {
+    max: 25,
+    speed: 400
+});
+
+/**script contato */
